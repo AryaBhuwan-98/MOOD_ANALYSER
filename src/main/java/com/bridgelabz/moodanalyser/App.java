9af994780ea9dbf323bedae87d2b777e7962b457
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-02-08 19:43:10 -0800

    Add:Welcome to the MOOD ANALYSER PROGRAM

---
+++ src/main/java/com/bridgelabz/moodanalyser/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.moodanalyser;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome TO the MOOD ANALYSER" );
+    }
+}
