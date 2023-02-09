fd061b0e9edeea65a3e671ca2f6743ce504ae06e
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-02-08 21:43:50 -0800

    Add:adding Test Case TO Handle Null Exception In Mood Analyser

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -7,15 +7,22 @@ public class MoodAnalyserTest {
     
     @Test
     public void testMoodAnalysis_whenMoodIsSad() {
-        MoodAnalyser moodAnalyzer = new MoodAnalyser("This is a sad message");//Tc1.1 Refactor
-        String mood = moodAnalyzer.analyseMood();
+        MoodAnalyser moodAnalyser = new MoodAnalyser("This is a sad message");//Tc1.1 Refactor
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals(mood,"SAD");
     }
 
     @Test
     public void testMoodAnalysis_whenMoodIsHappy() {//Tc1.2 Refactor
-        MoodAnalyser moodAnalyzer = new MoodAnalyser("This is a happy message");
-        String mood1 = moodAnalyzer.analyseMood();
+        MoodAnalyser moodAnalyser = new MoodAnalyser("This is a happy message");
+        String mood1 = moodAnalyser.analyseMood();
         Assert.assertEquals(mood1,"HAPPY");
     }
+    @Test
+    public void testMoodAnalysis_whenMoodIsNull() { //tc 2.1
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String mood2 = moodAnalyser.analyseMood();
+        Assert.assertEquals(mood2,"HAPPY");
+    }
 }
+    
