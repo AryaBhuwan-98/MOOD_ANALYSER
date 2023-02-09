542be717e0bb4d6a201a9e983366c4f6dd236b78
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-02-08 22:05:46 -0800

    Add:adding program to throw Null or Empty Mood Custom Exception

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -2,24 +2,25 @@ package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser {
     
-    private String message;
-    //Default Constructor
-    public MoodAnalyser() {
-    }
+    public String message;
+
     //Parameterized Constructor
     public MoodAnalyser(String message) {
         this.message = message;
     }
 
-    //Handle NULLPOINTER Exception using try-catch block
-    public String analyseMood() {
+    //Handle Customized Exception using try-catch block
+    public String analyseMood() throws MoodAnalysisException {
         try {
+            if (message.length() == 0)
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY , "Please enter proper message!");
             if (message.contains("sad"))
                 return "SAD";
             else
                 return "HAPPY";
-        } catch(Exception e) {
-            return "HAPPY";
+        } catch(NullPointerException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL , "Please enter proper message!");
         }
     }
 }
+    
--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -6,20 +6,20 @@ import org.junit.Test;
 public class MoodAnalyserTest {
     
     @Test
-    public void testMoodAnalysis_whenMoodIsSad() {
+    public void testMoodAnalysis_whenMoodIsSad() throws MoodAnalysisException {
         MoodAnalyser moodAnalyser = new MoodAnalyser("This is a sad message");//Tc1.1 Refactor
         String mood = moodAnalyser.analyseMood();
         Assert.assertEquals(mood,"SAD");
     }
 
     @Test
-    public void testMoodAnalysis_whenMoodIsHappy() {//Tc1.2 Refactor
+    public void testMoodAnalysis_whenMoodIsHappy() throws MoodAnalysisException {//Tc1.2 Refactor
         MoodAnalyser moodAnalyser = new MoodAnalyser("This is a happy message");
         String mood1 = moodAnalyser.analyseMood();
         Assert.assertEquals(mood1,"HAPPY");
     }
     @Test
-    public void testMoodAnalysis_whenMoodIsNull() { //tc 2.1
+    public void testMoodAnalysis_whenMoodIsNull() throws MoodAnalysisException { //tc 2.1
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
         String mood2 = moodAnalyser.analyseMood();
         Assert.assertEquals(mood2,"HAPPY");
